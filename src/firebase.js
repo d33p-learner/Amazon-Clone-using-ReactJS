6d945b5043c36f0f9b9f82dd50cf0d80d32ba237
Deep Raj <dpraj049@gmail.com>
2021-06-30 15:10:34 +0530

    first commit-Set up The Home Page and The Navbar

---
+++ src/firebase.js
@@ -0,0 +1,10 @@
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+    apiKey: "AIzaSyCP53nNyQxGysTts8jpQifvvKHKy69LkcI",
+    authDomain: "challange-6c24d.firebaseapp.com",
+    projectId: "challange-6c24d",
+    storageBucket: "challange-6c24d.appspot.com",
+    messagingSenderId: "847130926240",
+    appId: "1:847130926240:web:dabb6b274fe6355fa2bdc5",
+    measurementId: "G-B4KW65G669"
+  };
