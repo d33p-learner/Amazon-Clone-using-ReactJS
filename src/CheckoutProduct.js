6964a63d1e5525a83bd326866c91c1cf90cfeebb
Deep Raj <dpraj049@gmail.com>
2021-07-01 16:53:27 +0530

    Set up the checkout page, total price in cart, add and remove from basket functionalities

---
+++ src/CheckoutProduct.js
@@ -0,0 +1,36 @@
+import React from "react";
+import "./CheckoutProduct.css";
+import { useStateValue } from "./StateProvider";
+
+function CheckoutProduct({ id, title, image, price, rating }) {
+  const [state, dispatch] = useStateValue();
+  const { basket } = state;
+  const removeFromBasket = () => {
+    dispatch({
+      type: "REMOVE_FROM_BASKET",
+      id: id,
+    });
+  };
+  return (
+    <div className="checkoutProduct">
+      <img src={image} alt="" className="checkoutProduct__img" />
+      <div className="checkoutProduct__info">
+        <p className="checkoutProduct__title">{title}</p>
+        <p className="checkoutProduct__price">
+          <small>$</small>
+          {price}
+        </p>
+        <p className="checkoutProduct__rating">
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
+        </p>
+        <button onClick={removeFromBasket}>Remove From Basket</button>
+      </div>
+    </div>
+  );
+}
+
+export default CheckoutProduct;
--- src/Product.js
+++ src/Product.js
@@ -1,7 +1,28 @@
 import React from "react";
 import "./Product.css";
+import { useStateValue } from "./StateProvider";
 
 function Product({ id, title, image, price, rating }) {
+  // Pull information from the data layer
+  const [state, dispatch] = useStateValue();
+  const { basket } = state;
+
+  // console.log("Basket", basket);
+
+  const addToBasket = () => {
+    // dispatch the item to the data layer
+    dispatch({
+      type: "ADD_TO_BASKET",
+      item: {
+        id: id,
+        title: title,
+        image: image,
+        price: price,
+        rating: rating,
+      },
+    });
+  };
+
   return (
     <div className="product">
       <div className="product__info">
@@ -11,14 +32,15 @@ function Product({ id, title, image, price, rating }) {
           <strong>{price}</strong>
         </p>
         <div className="product__rating">
-          {Array(rating).fill().map((_, i) => (<p>⭐</p>))}
+          {Array(rating)
+            .fill()
+            .map((_, i) => (
+              <p>⭐</p>
+            ))}
         </div>
       </div>
-      <img
-        src={image}
-        alt=""
-      />
-      <button>Add To Basket</button>
+      <img src={image} alt="" />
+      <button onClick={addToBasket}>Add To Basket</button>
     </div>
   );
 }
